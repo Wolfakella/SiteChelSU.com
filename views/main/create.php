927373ceff848ef7750453b44baee523b811fa79
Arturoooooo <35744158+Arturchel174@users.noreply.github.com>
2018-04-10 23:11:56 +0500

    Update create.php

---
+++ views/main/create.php
@@ -0,0 +1,16 @@
+<?php
+use app\models\Students;
+use app\models\Teacher;
+use app\models\Subject;
+use app\models\Plus;
+use app\models\Group;
+use yii\helpers\Html;
+use yii\helpers\ArrayHelper;
+use yii\widgets\DetailView;
+use yii\grid\GridView;
+use yii\widgets\ActiveForm;
+?>
+<?
+  $visit;
+  echo 'Success';
+?>
